251b1458f3f14a297e87c6fc0de98f94142a6805
Panda <tuxmarcele@gmail.com>
2024-02-17 15:56:41 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,10 +17,53 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    //MÉTODO AUXILIAR
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
     //IMPLEMENTANDO MÉTODO FUNÇÃO
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI ACIMA
+        Position p = new Position(0, 0);
+        p.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI ESQUERDA
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI DIAGONAL ESQUERDA
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI DIREITA
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI DIAGONAL DIREITA
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MÉTODO FUNÇÃO MOVIMENTOS POSSIVEIS DO REI ABAIXO
+        p.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
         return mat;
     }
 }
