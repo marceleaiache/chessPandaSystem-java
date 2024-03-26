26eea6fa92bf87ff99502c8a722b84402b599806
Panda <tuxmarcele@gmail.com>
2024-03-26 17:20:13 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,19 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+
+    //DECLARAÇÃO DE ARGUMENTO DO OBJETO
+    private ChessMatch chessMatch;
+
     //MÉTODO CONSTRUTOR (repassando os dados da classe para a super classe)
-    public King(Board board, Color color) {
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     //MÉTODO TOSTRING
@@ -22,6 +28,13 @@ public class King extends ChessPiece {
         ChessPiece p = (ChessPiece)getBoard().piece(position);
         return p == null || p.getColor() != getColor();
     }
+
+    //MÉTODO FUNÇÃO AUXILIAR QUE TESTA SE A TORRE ESTÁ APTA PARA O ROOK
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+    }
+
     //IMPLEMENTANDO MÉTODO FUNÇÃO
     @Override
     public boolean[][] possibleMoves() {
@@ -77,6 +90,32 @@ public class King extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
+        //MÉTODO FUNÇÃO QUE VERIFICA O MOVIMENTO DO ROOK (SPECIALALMOVE CASTLING)
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            // SPECIALMOVE KINGSIDE ROOK
+            Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(posT1)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+        }
+
+        //MÉTODO FUNÇÃO QUE VERIFICA O MOVIMENTO DO ROOK (SPECIALALMOVE CASTLING)
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            // SPECIALMOVE QUEENSIDE ROOK
+            Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+            if (testRookCastling(posT2)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
+        }
         return mat;
     }
 }
